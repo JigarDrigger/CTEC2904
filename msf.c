2f45298919ec642fb3841f7983b6f270869a8c7e
Jigar Patel <pateljigar76@hotmail.com>
2018-12-05 23:14:02 +0000

    save

--- msf.c
+++ msf.c
@@ -33,3 +33,15 @@ int main()
 
 	return 0;
 }
+
+int main()
+{
+	int num1,num2,quotient;
+	
+	printf("Enter two numbers:");
+	scanf("%d %d",&num1,&num2);
+	
+	quotient=num1/num2;
+	printf("Quotient: %d",quotient);
+	return  0;
+}
